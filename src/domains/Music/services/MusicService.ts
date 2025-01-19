c9c51d5ef6ecb20d846661f85db6808cdf6d3778
João Lucas <jlucasbenhami@gmail.com>
2025-01-19 12:04:54 -0300

    feat: adição da implementação de music

---
+++ src/domains/Music/services/MusicService.ts
@@ -0,0 +1,52 @@
+import prisma from "../../../../database/prismaClient";
+import { Music } from "@prisma/client";
+
+class MusicService {
+  static async createMusic(body: Music) {
+    const music = {
+      title: body.title,
+      genre: body.genre,
+      coverPic: body.coverPic,
+      albumId: body.albumId,
+      artistId: body.artistId,
+    } as Music;
+
+    await prisma.music.create({ data: music });
+    return music;
+  }
+
+  static async getMusics() {
+    const musics = await prisma.music.findMany({
+      orderBy: { title: "asc" },
+    });
+    return musics;
+  }
+
+  static async getMusicById(requestedId: number) {
+    const music = await prisma.music.findFirst({
+      where: { id_Music: requestedId },
+    });
+    return music;
+  }
+
+  static async updateMusic(requestedId: number, body: Music) {
+    const updatedMusic = await prisma.music.update({
+      data: {
+        title: body.title,
+        genre: body.genre,
+        coverPic: body.coverPic,
+        albumId: body.albumId,
+        artistId: body.artistId,
+      },
+      where: { id_Music: requestedId },
+    });
+
+    return updatedMusic;
+  }
+
+  static async deleteMusic(requestedId: number) {
+    await prisma.music.delete({ where: { id_Music: requestedId } });
+  }
+}
+
+export default MusicService;
