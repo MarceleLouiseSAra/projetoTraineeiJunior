cfde20b03039b77a1d87487addef8388c971e118
Josias Daniel Sousa Ferreira <josiassousa9@gmail.com>
2025-01-19 16:05:34 -0300

    feat: adiciona pull request template

--- src/domains/Album/controller/AlbumController.ts
+++ src/domains/Album/controller/AlbumController.ts
@@ -1,1 +1,26 @@
-import AlbumService from "./src/domains/Album/services/AlbumService";
+import AlbumService from "../services/AlbumService";
+import { Album } from "@prisma/client";
+
+class AlbumController {
+  async criarAlbum(body: Album) {
+    return await AlbumService.createAlbum(body);
+  }
+
+  async getAlbums() {
+    return await AlbumService.getAlbums();
+  }
+
+  async getAlbumById(requestedId: number) {
+    return await AlbumService.getAlbumById(requestedId);
+  }
+
+  async atualizarAlbum(requestedId: number, body: Album) {
+    return await AlbumService.updateAlbum(requestedId, body);
+  }
+
+  async deletarAlbum(requestedId: number) {
+    await AlbumService.deleteAlbum(requestedId);
+  }
+}
+
+export default AlbumController;
