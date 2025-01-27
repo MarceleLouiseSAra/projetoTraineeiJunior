87e3dca440f8a2208f41e6023d79d0c4e2aa2d4e
Josias Daniel Sousa Ferreira <131787227+Leinadinho@users.noreply.github.com>
2025-01-26 23:08:57 -0300

    Update ArtistController.ts

--- src/domains/Artist/controller/ArtistController.ts
+++ src/domains/Artist/controller/ArtistController.ts
@@ -3,4 +3,66 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 
+router.get("/:get", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const artists = await ArtistService.getArtists();
+    res.json(artists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get(
+  "/:getById",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const artistById = await ArtistService.getArtistById(
+        Number(req.params.id),
+      );
+      res.json(artistById);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post(
+  "/:post",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const newArtist = await ArtistService.createArtist(req.body);
+      res.json(newArtist);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.put(
+  "/:update",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const updatedArtist = await ArtistService.updateArtist(
+        Number(req.params.update),
+        req.body,
+      );
+      res.json(updatedArtist);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.delete(
+  "/:delete",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await ArtistService.deleteArtist(Number(req.params.delete));
+      res.json();
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 export default router;
