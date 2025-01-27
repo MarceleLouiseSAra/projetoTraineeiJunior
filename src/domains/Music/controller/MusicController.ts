10bc996a9882e9430765b87561a9eccaee871d8e
Josias Daniel Sousa Ferreira <131787227+Leinadinho@users.noreply.github.com>
2025-01-26 23:11:22 -0300

    Update MusicController.ts

--- src/domains/Music/controller/MusicController.ts
+++ src/domains/Music/controller/MusicController.ts
@@ -3,4 +3,64 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 
+router.get("/:get", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const musics = await MusicService.getMusics();
+    res.json(musics);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get(
+  "/:getById",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const musicById = await MusicService.getMusicById(Number(req.params.id));
+      res.json(musicById);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post(
+  "/:post",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const newMusic = await MusicService.createMusic(req.body);
+      res.json(newMusic);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.put(
+  "/:update",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const updatedMusic = await MusicService.updateMusic(
+        Number(req.params.update),
+        req.body,
+      );
+      res.json(updatedMusic);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.delete(
+  "/:delete",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await MusicService.deleteMusic(Number(req.params.delete));
+      res.json();
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 export default router;
